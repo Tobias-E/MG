7b33365cda6a70e41d91c4354c9dcacd5131ea31
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-12-14 20:36:31 +0100

    test

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -37,7 +37,7 @@ const Wrapper = styled.div`
 `;
 
 const Background = styled.div`
-	-webkit-transform: translate3d() (0, 0, 0);
+	-webkit-transform: translate3d(0, 0, 0);
 	transform: translate3d(0, 0, 0);
 	width: 100vw;
 	position: fixed;
