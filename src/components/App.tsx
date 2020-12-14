c7c26953da321aa8cbb698ee720ffd0454a8af59
HappyHomie <47347121+HappyHomie@users.noreply.github.com>
2020-12-14 16:42:43 +0100

    test is still on

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -40,11 +40,11 @@ const Background = styled.div`
 	width: 100vw;
 	position: fixed;
 	z-index: -100;
-	/* 	top: 0;
-	bottom: 0; */
+	top: 0;
+	bottom: 0;
 	left: 0;
 	right: 0;
-	margin: auto;
+	margin: 0 auto;
 	opacity: 0.5;
 	background-image: url(${logo});
 	background-position: center;
